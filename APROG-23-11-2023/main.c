8af1d356a1fd02ccd4973c96fdd2c0f6fe55a59d
Vasco Magolo <vascomagolo@gmail.com>
2023-11-23 12:51:21 +0000

    corrected

--- APROG-23-11-2023/main.c
+++ APROG-23-11-2023/main.c
@@ -1,25 +1,44 @@
 #include <stdio.h>
 
 float m(int n, int *v){
-    int soma=0;
-    for(int i=0;i<n;i++){
-        soma+=v[i];
+    int soma = 0;
+    for (int i = 0; i < n; i++){
+        soma += v[i];
     }
-    return (float)soma/n;
+    return (float)soma / n;
 }
+
 void LV(int n, int *v){
-    for(int i=0;i<n;i++){
+    for (int i = 0; i < n; i++){
         printf("Digite um numero: ");
-        scanf("%d",&v[i]);
+        scanf("%d", &v[i]);
     }
 }
+
+int encontrarmaior(int n, int *v){
+    int maior = 0;
+    for (int i = 1; i < n; i++){
+        if (v[i] > v[maior]){
+            maior = i;
+        }
+    }
+    return maior; 
+}
+
 int main() {
-    int soma=0,vet[]={};
     int count;
     printf("Digite quantos numeros deseja digitar: ");
-    scanf("%d",&count);
-    LV(count,vet);
-    float media=m(count,vet);
-    printf("A media dos numeros digitados e: %.2f",media);
+    scanf("%d", &count);
+
+    int vet[count];
+
+    LV(count, vet);
+
+    float media = m(count, vet);
+    printf("A media dos numeros digitados e: %.2f \n", media);
+
+    int posicao_maior = encontrarmaior(count, vet);
+    printf("O maior numero digitado e: %d", vet[posicao_maior]);
+
     return 0;
 }
